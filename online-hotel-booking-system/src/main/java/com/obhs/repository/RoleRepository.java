ce036b9647b626b8a1b3d69786bdd3f675923015
pvsnarsimha <114327962+pvsnarsimha@users.noreply.github.com>
2025-05-21 16:10:40 +0530

    Update RoleRepository.java

--- online-hotel-booking-system/src/main/java/com/obhs/repository/RoleRepository.java
+++ online-hotel-booking-system/src/main/java/com/obhs/repository/RoleRepository.java
@@ -1,8 +1,12 @@
 package com.obhs.repository;
+
 import com.obhs.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Optional<Role> findByName(String name);
 }
